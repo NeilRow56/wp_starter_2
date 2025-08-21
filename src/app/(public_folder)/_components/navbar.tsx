a892ea2230c12233d243b331a35ff6b0f865b852
NeilRow56 <neilrowland56@gmail.com>
2025-08-21 11:56:09 +0100

    first commit - wp_starter_1 plus public folder with layout and navbar

---
+++ src/app/(public_folder)/_components/navbar.tsx
@@ -0,0 +1,84 @@
+'use client'
+
+import { buttonVariants } from '@/components/ui/button'
+
+import Image from 'next/image'
+import Link from 'next/link'
+
+import { useSession } from '@/lib/auth-client'
+import { UserDropdown } from './user-dropdown'
+import { ModeToggle } from '@/components/mode-toggle'
+import { APP_NAME } from '@/lib/constants'
+import { GetStartedButton } from '@/components/get-started'
+
+interface navigationItemsProps {
+  name: string
+  href: string
+}
+
+const navigationItems: navigationItemsProps[] = [
+  //   {
+  //     name: 'Courses',
+  //     href: '/courses'
+  //   },
+  {
+    name: 'Dashboard',
+    href: '/admin/dashboard'
+  }
+]
+
+export function Navbar() {
+  const { data: session, isPending } = useSession()
+  return (
+    <header className='bg-background/95 backdrop-blur-[backdrop-filter]:bg-background sticky top-0 z-50 w-full border-b'>
+      <div className='container mx-auto flex min-h-16 items-center px-4 md:px-6 lg:px-8'>
+        <Link href='/' className='mr-4 flex items-center space-x-2'>
+          <Image
+            src={'/logo.svg'}
+            alt='logo'
+            width={32}
+            height={32}
+            className='size-9'
+          />
+          <span className='font-bold'>{APP_NAME}</span>
+        </Link>
+        {/* Desktop navigation */}
+        <nav className='hidden md:flex md:flex-1 md:items-center md:justify-between'>
+          <div className='flex items-center gap-2'>
+            {navigationItems.map(item => (
+              <Link
+                className='hover:text-primary text-sm font-medium transition-colors'
+                href={item.href}
+                key={item.href}
+              >
+                {item.name}
+              </Link>
+            ))}
+          </div>
+          <div className='flex items-center space-x-4'>
+            <ModeToggle />
+            {isPending ? null : session ? (
+              <UserDropdown
+                name={session.user.name}
+                email={session.user.email}
+                image={session.user.image || ''}
+              />
+            ) : (
+              <>
+                <Link
+                  href='/sign-in'
+                  className={buttonVariants({
+                    variant: 'secondary'
+                  })}
+                >
+                  Login
+                </Link>
+                <GetStartedButton />
+              </>
+            )}
+          </div>
+        </nav>
+      </div>
+    </header>
+  )
+}
---
+++ src/app/(public_folder)/layout.tsx
@@ -0,0 +1,14 @@
+import { Navbar } from '@/app/(public_folder)/_components/navbar'
+
+export default async function PublicFolderLayout({
+  children
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <div className=''>
+      <Navbar />
+      <main className='container mx-auto px-4 md:px-6 lg:px-8'>{children}</main>
+    </div>
+  )
+}
